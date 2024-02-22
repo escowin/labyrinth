bf48771e241fdcd0dbf30e30bb75ceec9b0bdd4a
escowin <edwin@escowinart.com>
2024-02-22 13:02:31 -0600

    used various data types to display multi-line message in terminal

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,10 +3,39 @@
 
 int main()
 {
+    // Variables | Declarations & assignments
+
+    // Data types
+    // integer | whole numbers
+    int x;
+    x = 5;
+
+    int y = 10;
+    int sum = x + y;
+    int year = 2024;
+
+    // double | decimals
+    double ver = 1.01;
+
+    // char | single character, uses single quotes
+    char grade = 'A';
+
+    // boolean | true or false
+    bool student = true;
+
+    // string | multi character text, uses double quotes
+    std::string appName = "Labyrinth";
+    std::string name = "Edwin Escobar";
+
     // Multi-line terminal message
-    // - standard :: character output << "string" << end line
-    std::cout << "Hello world" << std::endl;
-    std::cout << "C++ stuff" << std::endl;
+    // - standard :: character output << "string" << end line or '\n'; repeat
+    std::cout << appName << " v" << ver << std::endl;
+    std::cout << "\u00A9 " << year << " " << name << '\n';
+
+    // Displays variable
+    std::cout << x << '\n';
+    std::cout << y << '\n';
+    std::cout << sum << '\n';
 
     // Return 0 indicates no errors
     return 0;
