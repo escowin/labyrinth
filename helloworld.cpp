72b03333083ed6d51506a3c2b20cb359426db7bc
escowin <edwin@escowinart.com>
2024-02-22 12:21:21 -0600

    printed multi-line string in the terminal

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+// Loads header file for basic input & output operations
+#include <iostream>
+
+int main()
+{
+    // Multi-line terminal message
+    // - standard :: character output << "string" << end line
+    std::cout << "Hello world" << std::endl;
+    std::cout << "C++ stuff" << std::endl;
+
+    // Return 0 indicates no errors
+    return 0;
+}
