fec3d7e7c1bc901c3a99d5619963e3f072377ff7
escowin <edwin@escowinart.com>
2024-02-22 20:26:59 -0600

    tested standard & cmath arithmetic functions; cleaned up pseudocode

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,13 +1,10 @@
 // Loads header file for basic input & output operations
 #include <iostream>
-#include <vector>
+#include <cmath>
 
-// type def convention ends in `_t`
-// typedef std::vector<std::pair<std::string, int>> pairlist_t
+// type def convention ends in `_t`.
 // typedef std::string text_t;
-
-// more common than typedef since its more suitable for templates
-using text_t = std::string;
+using text_t = std::string; // more suitable than typedef for templates
 
 // namespaces provides a solution for preventing nam conflicts in large projects
 namespace first
@@ -15,14 +12,9 @@ namespace first
     int x = 1;
 }
 
-namespace second
-{
-    int x = 2;
-}
-
 int main()
 {
-    using namespace first;
+    // using namespace first;
     using std::cin;
     using std::cout;
 
@@ -31,35 +23,30 @@ int main()
     double ver = 1.01;   // decimals
     bool student = true; // true or false
     char grade = 'A';    // single character, uses single quotes
-    int age;
 
     // string | multi character text object, uses double quotes
     text_t appName = "Labyrinth";
     text_t name = "Edwin Escobar";
-    text_t fullName;
-
-    const double PI = 3.14159; // const locks value, use all caps for convention
-    double r = 10;
-    double circ = 2 * PI * r;
-
-    // int x = 0;
 
     // Terminal messages
     cout << appName << " v" << ver << std::endl;
-    cout << "\u00A9 " << year << " " << name << '\n';
-    cout << "circle circumference: " << circ << '\n';
-    cout << x << '\n';
+    cout << "\u00A9 " << year << " " << name << "\n\n";
 
-    // Interactive terminal prompt
-    cout << "What's your age? ";
-    cin >> age;
+    // Arithmetic | https://cplusplus.com/reference/cmath/
+    double x = 3.99;
+    double y = 4;
+    double z;
 
-    cout << "What's your full name? ";
-    // Allows strings w/ spaces to be captured. ::ws removes any preceding new line or user input white space
-    std::getline(cin >> std::ws, fullName);
+    // z = std::max(x, y);
+    // z = std::min(x, y);
+    // z = pow(2, 4); // power
+    // z = sqrt(9);   // square root
+    // z = abs(-3);   // absolute
+    // z = round(x);  // round
+    // z = ceil(x);   // round up
+    z = floor(x); // round down
 
-
-    cout << "Hello, " << fullName << "! You are " << age << " years old";
+    cout << z;
 
     return 0;
 }
