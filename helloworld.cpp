088bb903b3c73b0e112785718564aac530bd4e6d
escowin <edwin@escowinart.com>
2024-02-22 21:05:40 -0600

    cleaned up code organization

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,27 +5,27 @@
 // type def convention ends in `_t`.
 // typedef std::string text_t;
 using text_t = std::string; // more suitable than typedef for templates
+using std::cin;
+using std::cout;
 
 int main()
 {
-    using std::cin;
-    using std::cout;
-
-    // App variables | Data type declarations & assignments
-    int year = 2024;     // whole number
-    double ver = 1.02;   // decimal
-    text_t appName = "Labyrinth"; // string
+    // App details | Data type declarations & assignments
+    int year = 2024;               // whole number
+    double ver = 1.03;             // decimal
+    text_t appName = "labyrinth";  // string
     text_t name = "Edwin Escobar"; // string
-    text_t subhead = "Hypotenuse calculator";
+    text_t subhead = "hypotenuse-calculator";
 
-    // Terminal greeting
-    cout << appName << " v" << ver << " | " << subhead << std::endl;
-    cout << "\u00A9 " << year << " " << name << "\n\n";
-    
-    // Hypotenuse calculator | c = √(a^2 + b2)
+    // Hypotenuse calculator | c = √(a^2 + b^2)
     double a;
     double b;
     double c;
+
+    // Terminal greeting
+    cout << appName << " v" << ver << std::endl;
+    cout << "\u00A9 " << year << " " << name << "\n\n";
+    cout << subhead << '\n';
 
     // Terminal prompt
     cout << "Enter side A: ";
@@ -34,6 +34,7 @@ int main()
     cout << "Enter side B: ";
     cin >> b;
 
+    // Calculates solution with provided user input, displaying the solution in the terminal
     c = sqrt(pow(a, 2) + pow(b, 2));
 
     cout << "Side C: " << c;
