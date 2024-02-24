28456fea49469940c80df0d6515c3975825a6cb8
escowin <edwin@escowinart.com>
2024-02-23 22:01:39 -0600

    passed user input through if... else conditionals, arithmetic, and OR operator to convert temp units

--- helloworld.cpp
+++ helloworld.cpp
@@ -15,24 +15,43 @@ int main()
     double ver = 1.05;
     text_t appName = "labyrinth";
     text_t name = "Edwin Escobar";
-    text_t subhead = "logical operators";
+    text_t subhead = "temperature-calculator";
 
     // Variables
-    int temp;
-    bool sunny = true;
+    double temp;
+    char unit;
 
     // Terminal greeting
     cout << appName << " v" << ver << std::endl;
     cout << "\u00A9 " << year << " " << name << "\n\n";
     cout << "********** " << subhead << " **********\n\n";
+    cout << "F = Fahrenheit\n";
+    cout << "C = Celsius\n";
 
     // Terminal prompts
-    cout << "Enter temp: ";
-    cin >> temp;
+    cout << "Select a unit to convert to: ";
+    cin >> unit;
 
-    (temp <= 0 || temp >= 30) ? cout << "temp is bad\n" : cout << "temp is good\n";
+    if (unit == 'F' || unit == 'f')
+    {
+        cout << "Enter temp in Celsius: ";
+        cin >> temp;
 
-    !sunny ? cout << "it is sunny" : cout << "it is cloudy";
+        temp = (1.8 * temp) + 32.0;
+        cout << "Temp is: " << temp << "F\n";
+    }
+    else if (unit == 'C' || unit == 'c')
+    {
+        cout << "Enter temp in Fahrenheit: ";
+        cin >> temp;
+
+        temp = (temp - 32) / 1.8;
+        cout << "Temp is: " << temp << "C\n";
+    }
+    else
+    {
+        cout << "Invalid unit, enter either C or F\n";
+    }
 
     cout << "\n\n********************************\n";
 
