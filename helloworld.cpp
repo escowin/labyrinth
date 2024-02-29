4fceb13abd938e4ac5c4a13c72b6a8bc319704f0
escowin <edwin@escowinart.com>
2024-02-29 11:07:03 -0600

    passed random number value through switch case to display a terminal message

--- helloworld.cpp
+++ helloworld.cpp
@@ -17,27 +17,41 @@ int main()
 {
     // Variables | Data type declarations & assignments
     int year = 2024;
-    double ver = 1.12;
+    double ver = 1.13;
     text_t appName = "white-raspberry";
-    text_t subhead = "random number generator";
+    text_t subhead = "random super power generator";
     // int dice;
 
-    // pseudo-random | initialize random fx, seed w/ current calendar time
-    srand(time(NULL));
+    // pseudo-random | uses the current time to generate random numbers
+    srand(time(0));
 
-    // Returns a random number between 1-6 with arithemetic
-    int num1 = (rand() % 6) + 1;
-    int num2 = (rand() % 6) + 1;
-    int num3 = (rand() % 6) + 1;
+    // Returns a random number between 1-5 with arithemetic
+    int num1 = (rand() % 5) + 1;
 
     // Terminal greeting
     cout << appName << " v" << ver << std::endl;
     cout << "\u00A9 " << year << " Edwin Escobar\n\n";
     cout << "********** " << subhead << " **********\n\n";
 
-    // Terminal prompts
-    cout << "rolling the dice: ";
-    cout << num1 << " | " << num2 << " | " << num3;
+    // Terminal message
+    switch (num1)
+    {
+    case 1:
+        cout << "You get za waruldo!\n";
+        break;
+    case 2:
+        cout << "You get spidey sense\n";
+        break;
+    case 3:
+        cout << "You get blueshell\n";
+        break;
+    case 4:
+        cout << "You get time travel\n";
+        break;
+    case 5:
+        cout << "You get nothing\n";
+        break;
+    }
 
     cout << "\n\n********** " << subhead << " **********\n\n";
 
