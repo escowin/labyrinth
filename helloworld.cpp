e72f41ef2e13d64712ca0a60cd6aa9c0db990255
escowin <edwin@escowinart.com>
2024-02-29 11:17:50 -0600

    implemented interactive guessing game with do while loop, cin, and if conditionals

--- helloworld.cpp
+++ helloworld.cpp
@@ -19,40 +19,42 @@ int main()
     int year = 2024;
     double ver = 1.13;
     text_t appName = "white-raspberry";
-    text_t subhead = "random super power generator";
-    // int dice;
-
-    // pseudo-random | uses the current time to generate random numbers
-    srand(time(0));
+    text_t subhead = "number guessing game";
 
     // Returns a random number between 1-5 with arithemetic
-    int num1 = (rand() % 5) + 1;
+    int num;
+    int guess;
+    int tries;
+
+    // pseudo-random | uses the current time to generate random numbers
+    srand(time(NULL));
+    num = (rand() % 100) + 1;
 
     // Terminal greeting
     cout << appName << " v" << ver << std::endl;
     cout << "\u00A9 " << year << " Edwin Escobar\n\n";
     cout << "********** " << subhead << " **********\n\n";
 
-    // Terminal message
-    switch (num1)
+    // Terminal prompts
+    do
     {
-    case 1:
-        cout << "You get za waruldo!\n";
-        break;
-    case 2:
-        cout << "You get spidey sense\n";
-        break;
-    case 3:
-        cout << "You get blueshell\n";
-        break;
-    case 4:
-        cout << "You get time travel\n";
-        break;
-    case 5:
-        cout << "You get nothing\n";
-        break;
-    }
+        cout << "Guess a number between (1-100): ";
+        cin >> guess;
+        tries++;
 
+        if (guess > num)
+        {
+            cout << "Too high!\n";
+        }
+        else if (guess < num)
+        {
+            cout << "Too low!\n";
+        }
+        else
+        {
+            cout << "Correct! # of tries: " << tries << '\n';
+        }
+    } while (guess != num);
     cout << "\n\n********** " << subhead << " **********\n\n";
 
     return 0;
