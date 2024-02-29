fa0df54cddfa130fc4af9c49d583fe123703a7c9
escowin <edwin@escowinart.com>
2024-02-29 15:58:38 -0600

    modularized terminal greeting & associated variables

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,72 +1,91 @@
+// Git commands
+// compile  | g++ -o program_name source_file.cpp
+// invoke   | ./program_name.exe
+
 // Loads header file for basic input & output operations
 #include <iostream>
 #include <cmath>
 #include <ctime>
 
 // type def convention ends in `_t`.
-// typedef std::string text_t;
-using text_t = std::string; // more suitable than typedef for templates
+// typedef std::string string_t;
+using string_t = std::string; // more suitable than typedef for templates
 using std::cin;
 using std::cout;
 
-// Git commands
-// compile  | g++ -o program_name source_file.cpp
-// invoke   | ./program_name.exe
-
 // User declared functions | parameters list data type followed by variable name
+int currentYear();
 double square(double length);
 double cube(double length);
+void terminalStart(string_t subhead);
 void terminalFin(int length);
 
 int main()
 {
     // Variables | Data type declarations & assignments
-    int year = 2024;
-    double ver = 1.15;
-    text_t appName = "white-raspberry";
-    text_t subhead = "geometry-calculator";
+    string_t subhead = "geometry-calculator";
     double length;
-    double area;
-    double volume;
 
     // Terminal greeting
-    cout << appName << " v" << ver << std::endl;
-    cout << "\u00A9 " << year << " Edwin Escobar\n\n";
-    cout << "********** " << subhead << " **********\n\n";
+    terminalStart(subhead);
 
     // Terminal prompts
     cout << "Enter the length of the square/cube: ";
     cin >> length;
 
-    area = square(length);
-    volume = cube(length);
+    double area = square(length);
+    double volume = cube(length);
 
     cout << "2D Area: " << area << "cm^2\n";
     cout << "3D Volume: " << volume << "cm^3\n";
 
     // Terminal fin
-    cout << "\n\n***********";
     terminalFin(subhead.length());
-    cout << "***********\n\n";
-
     return 0;
 }
 
-// User defined functions
+// Returns calculated 2D area
 double square(double length)
 {
     return pow(length, 2);
 }
 
+// Returns calculated 3D volume
 double cube(double length)
 {
     return pow(length, 3);
 }
 
+// Returns the current year
+int currentYear() {
+    // The current time in seconds since the Unix Epoch
+    std::time_t currentTime = std::time(nullptr);
+    // Converts current time to tm structure
+    std::tm* timeInfo = std::localtime(&currentTime);
+    // Extracts current year from tm structure
+    int currentYear = timeInfo->tm_year + 1900;
+    return currentYear;
+}
+
+// Greets user with app details in the terminal
+void terminalStart(string_t subhead)
+{
+    int year = currentYear();
+    double ver = 1.16;
+    string_t appName = "white-raspberry";
+
+    cout << appName << " v" << ver << std::endl;
+    cout << "\u00A9 " << year << " Edwin Escobar\n\n";
+    cout << "********** " << subhead << " **********\n\n";
+}
+
+// Signals app's conclusion in the terminal
 void terminalFin(int length)
 {
+    cout << "\n\n***********";
     for (int i = 0; i < length; i++)
     {
         cout << "*";
     }
+    cout << "***********\n\n";
 }
